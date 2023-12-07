81b8e6fa54f84ae1e0949e2daa7f5405e40a1946
dominicpam89 <dominicpam89@gmail.com>
2023-12-07 09:24:20 +0700

    added content home section-2

--- src/features/Home.tsx
+++ src/features/Home.tsx
@@ -1,9 +1,11 @@
 import { redirect } from "react-router-dom"
 import HomeSection1 from "./Home/Section-1";
+import HomeSection2 from "./Home/Section-2";
 
 const HomePage = () => {
   return <>
     <HomeSection1 />
+    <HomeSection2 />
   </>
 }
 
--- src/features/Home/Section-2.tsx
+++ src/features/Home/Section-2.tsx
@@ -1,6 +1,11 @@
 import { useContext } from "react"
 import { useHooksMediaQuery } from "../../data/hooks/media-query"
 import { ContextMain } from "../../data/context/main"
+import { IconContext } from "react-icons"
+import { BsBullseye } from "react-icons/bs"
+import { BsCalendar2RangeFill } from "react-icons/bs"
+import { BsFillHddRackFill } from "react-icons/bs"
+
 
 const HomeSection2 = () => {
 	const context = useContext(ContextMain)
@@ -9,7 +14,7 @@ const HomeSection2 = () => {
 	desktopView
 
 	const twClasses = {
-		container: `w-full px-12 py-24 flex flex-row space-x-5 items-center justify-center bg-gradient-to-b from-primary-dark-color to-primary-main-color text-primary-main-contrast`,
+		container: `w-full px-16 py-24 flex flex-col space-y-5 items-center justify-center bg-gradient-to-b from-primary-dark-color to-primary-main-color text-primary-main-contrast`,
 		
 	}
 
@@ -19,8 +24,26 @@ const HomeSection2 = () => {
 				id="section-2"
 				className={twClasses.container}
 			>
-				<div>
-						
+				<div id="column-group" className="">
+						<IconContext.Provider value={{}}>
+							<BsBullseye />
+						</IconContext.Provider>
+						<h2>Custom challenge creation</h2>
+						<p>Set the rules, define your own pace</p>
+				</div>		
+				<div id="column-group" className="">
+						<IconContext.Provider value={{}}>
+							<BsCalendar2RangeFill />
+						</IconContext.Provider>
+						<h2>Track the progress</h2>
+						<p>See your growth over time with our analytics tools</p>
+				</div>		
+				<div id="column-group" className="">
+						<IconContext.Provider value={{}}>
+							<BsFillHddRackFill />
+						</IconContext.Provider>
+						<h2>Community Support</h2>
+						<p>Join our community and get motivated by peers</p>
 				</div>		
 			</div>
 		</>

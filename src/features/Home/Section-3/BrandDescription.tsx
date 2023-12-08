8a71d807e81e67b1fad1ab3bf89c98798848e154
dominicpam89 <dominicpam89@gmail.com>
2023-12-08 12:20:59 +0700

    fix text in section 3

--- src/features/Home/Section-3/BrandDescription.tsx
+++ src/features/Home/Section-3/BrandDescription.tsx
@@ -6,17 +6,18 @@ const HW: React.FC<{ children: React.ReactNode }> = ({ children }) => {
 	return <span className="text-info-light-color">{children}</span>
 }
 
-export default function Section3_BrandDescription(){
+export default function Section3_BrandDescription() {
 	const context = useContext(ContextMain)
 	const descriptionFont =
 		context.theme.current === "theme-relax" ? "font-medium" : "font-thin"
 	const twClass = `${descriptionFont} py-2 text-sm text-primary-main-contrast text-justify hyphens-auto break-words leading-6`
 	return (
 		<p className={twClass}>
-			Fancydo is Dominic Pam showcase of Front End Stack Technology mainly{" "}
-			<HW>React</HW>, <HW>React Router</HW>, <HW>Tanstack</HW>, and using
-			Supabase as backend service. As other library for UI such as{" "}
-			<HW>React Icons</HW>, <HW>Tailwind</HW> and <HW>Framer Motion</HW>
+			Fancydo is DominicPam's showcase built with <HW>SOLID principle</HW> in
+			Front End Stack Technology mainly <HW>React</HW>, <HW>React Router</HW>
+			, <HW>Tanstack</HW>, and using Supabase as backend service. UI Library
+			that also included are <HW>React Icons</HW>, <HW>Tailwind</HW> and{" "}
+			<HW>Framer Motion</HW> for animation
 		</p>
 	)
 }

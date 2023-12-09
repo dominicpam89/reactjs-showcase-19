ffa9fb27ac517fcf9f0c282117a2372a02a9da18
dominicpam89 <dominicpam89@gmail.com>
2023-12-09 22:46:29 +0700

    moved UIModal component to ...Todo/Form.tsx

--- src/features/Todo/Form.tsx
+++ src/features/Todo/Form.tsx
@@ -1,27 +1,32 @@
 import { useForm, FieldErrors, FieldValues } from "react-hook-form"
 import UIInputField from "../../UI/Form/InputField"
 import UIInputFieldArea from "../../UI/Form/InputFieldArea"
+import UIModal from "../../UI/Modal"
 import { utilsTodoFormDefaultValues as defaultValues } from "../../data/utils/todoForm"
 
 const TodoForm = () => {
-	const { register, handleSubmit } = useForm({
-		defaultValues
-	})
-	const onSubmit = (data:FieldValues)=>{
+	const { register, handleSubmit } = useForm({ defaultValues })
+	const onSubmit = (data: FieldValues) => {
 		console.log(data)
 	}
-	const onInvalid= (errors:FieldErrors)=>{
+	const onInvalid = (errors: FieldErrors) => {
 		console.log(errors)
 	}
 	return (
-		<form
-			className="w-full h-full p-10 flex flex-col space-y-4 bg-primary-main-contrast"
-			onClick={(e) => e.stopPropagation()}
-			onSubmit={handleSubmit(onSubmit,onInvalid)}
-		>
-			<UIInputField id="tag" label="Todo Title" register={register("tag")} />
-			<UIInputFieldArea id="description" label="Description" rows={4} />
-		</form>
+		<UIModal padding="sm">
+			<form
+				className="w-full p-10 flex flex-col space-y-4 bg-primary-main-contrast"
+				onClick={(e) => e.stopPropagation()}
+				onSubmit={handleSubmit(onSubmit, onInvalid)}
+			>
+				<UIInputField
+					id="tag"
+					label="Todo Title"
+					register={register("tag")}
+				/>
+				<UIInputFieldArea id="description" label="Description" rows={5} />
+			</form>
+		</UIModal>
 	)
 }
 

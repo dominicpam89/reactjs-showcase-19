8c9ba65b8dcf0e8efab26ef1967325d9d55e53e2
dominicpam89 <dominicpam89@gmail.com>
2023-12-09 22:20:59 +0700

    established UI/Form/Input

--- src/UI/Form/InputFieldArea.tsx
+++ src/UI/Form/InputFieldArea.tsx
@@ -1,13 +1,14 @@
-import { TypeInputField } from "../../data/types/Props"
+import { TypeInputFieldArea } from "../../data/types/Props"
 import UIInputHelperText from "./HelperText"
 
-const UIInputField:React.FC<TypeInputField> = ({id,label,type="text",register, error=undefined}) => {
+
+const UIInputFieldArea:React.FC<TypeInputFieldArea> = ({id, label, rows=3, register=undefined, error=undefined}) => {
 	return (
 		<>
 			<div id="form-input-group" className="relative">
-				<input
-					type={type}
+				<textarea
 					id={id}
+					rows={rows}
 					className="block px-2.5 pb-2.5 pt-4 w-full text-sm 
               text-primary-main-color bg-transparent rounded-sm border border-primary-main-color appearance-none
               focus:outline-none focus:ring-1 focus:border-1 focus:border-primary-main-color peer"
@@ -19,8 +20,8 @@ const UIInputField:React.FC<TypeInputField> = ({id,label,type="text",register, e
 					className="absolute top-2 origin-[0] px-2
             text-sm text-primary-main-color duration-300 transform -translate-y-4 scale-75 
             bg-primary-main-contrast
-            peer-focus:px-2 peer-focus:text-primary-main-color  
-            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 
+            peer-focus:px-2 peer-focus:text-primary-main-color
+            peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/4 
             peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 
             peer-focus:start-4
             rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto start-1"
@@ -28,9 +29,9 @@ const UIInputField:React.FC<TypeInputField> = ({id,label,type="text",register, e
 					{label}
 				</label>
 			</div>
-			<UIInputHelperText text={error} />
+      <UIInputHelperText text={error} />
 		</>
 	)
 }
 
-export default UIInputField
+export default UIInputFieldArea

f3eb756576f39d48b647d52f0478b25629f8a66a
dominicpam89 <dominicpam89@gmail.com>
2023-12-09 22:19:35 +0700

    added default values to utils/todoForm.ts

---
+++ src/data/utils/todoForm.ts
@@ -0,0 +1,8 @@
+import { TypeTodoFormDefaultValues } from "../types/useForm"
+
+export const defaultValues:TypeTodoFormDefaultValues = {
+  tag: "",
+  details: "",
+  dateFinished: new Date(),
+  images: []
+}

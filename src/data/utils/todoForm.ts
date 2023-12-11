0a50ff35e18da72a6e7ac739fe269d9b96853589
dominicpam89 <dominicpam89@gmail.com>
2023-12-11 21:08:27 +0700

    improve utils/todoForm.ts

--- src/data/utils/todoForm.ts
+++ src/data/utils/todoForm.ts
@@ -1,13 +1,21 @@
-interface TypeTodoFormDefaultValues {
+export type TypeTodoFormDefaultValues = {
   tag: string
 	details: string
-	dateFinished: Date
-	images: string
+	dateFinished: string
+	image: string
 }
+
+const now = new Date()
+	const dateVal = {
+		year: now.getFullYear(),
+		month: now.getMonth()+1,
+		date: now.getDate(),
+	}
+const formattedDate = `${dateVal.year}-${dateVal.month}-${dateVal.date}`
 
 export const utilsTodoFormDefaultValues:TypeTodoFormDefaultValues = {
   tag: "",
   details: "",
-  dateFinished: new Date(),
-  images: ""
+  dateFinished: formattedDate,
+  image: "",
 }

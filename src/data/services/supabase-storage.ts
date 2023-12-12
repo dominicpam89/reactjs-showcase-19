18b9d9425f38aad733c252958cc8c97a5eb9730b
dominicpam89 <dominicpam89@gmail.com>
2023-12-12 13:20:39 +0700

    added query in new storage/activity items

--- src/data/services/supabase-storage.ts
+++ src/data/services/supabase-storage.ts
@@ -17,3 +17,9 @@ export const getImagesThemeEarth = async()=>{
   if(error) throw new Error("Couldn't access theme earth storage")
   return images
 }
+
+export const getActivityImages = async()=>{
+  const {data:images, error} = await supabase.storage.from("activity").list()
+  if(error) throw new Error("Couldn't access image files in storage")
+  return images.filter(image=>image.name.includes("png"))
+}

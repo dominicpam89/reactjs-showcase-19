1784b1d83e7f9542352f1ce8f3c8b73ab7685235
dominicpam89 <dominicpam89@gmail.com>
2023-12-12 13:42:23 +0700

    setup Todo/Item.tsx

--- src/features/Todo/Item.tsx
+++ src/features/Todo/Item.tsx
@@ -1,7 +1,6 @@
 import { TypeTodo } from "../../data/types/query";
 
 const TodoItem:React.FC<{todo:TypeTodo}> = ({todo}) => {
-  console.log(todo)
   return <>
     <div id="container" className="w-full grid grid-cols-3">
       

d3f2cf84ea4abd24d955416e6576821d7ff2408a
dominicpam89 <dominicpam89@gmail.com>
2023-12-15 20:10:22 +0700

    added delete query in services/todos.ts

--- src/data/services/todos.ts
+++ src/data/services/todos.ts
@@ -1,6 +1,7 @@
 import { supabase } from "./supabase"
 import { TypeTodo } from "../types/query"
 
+
 export const getTodos = async ()=>{
   const { data, error } = await supabase
     .from('todos')
@@ -8,4 +9,17 @@ export const getTodos = async ()=>{
   if(error) throw new Error("Couldn't get todos from database")
   const todos: TypeTodo[] = data
   return todos
+}
+
+
+export const addTodo = async()=>{
+  
+}
+
+export const deleteTodo = async(todoId:number)=>{
+  const { error } = await supabase
+  .from('todos')
+  .delete()
+  .eq('id', todoId)
+  if(error) throw new Error("Couldn't delete this item!")
 }

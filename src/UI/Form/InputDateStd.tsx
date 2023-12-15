18d6d3f5e877b31ee0a576309329e3f1b131ee92
dominicpam89 <dominicpam89@gmail.com>
2023-12-15 20:10:59 +0700

    added minimum restraint in InputDateStd.tsx

--- src/UI/Form/InputDateStd.tsx
+++ src/UI/Form/InputDateStd.tsx
@@ -1,4 +1,5 @@
 import { TypeInput } from "../../data/types/Props"
+import { getFormDate } from "../../data/utils/todoForm"
 import UIInputHelperText from "./HelperText"
 
 const UIInputDateStd:React.FC<TypeInput> = ({id, label, register=undefined, error=undefined}) => {
@@ -9,6 +10,7 @@ const UIInputDateStd:React.FC<TypeInput> = ({id, label, register=undefined, erro
 				id={id}
 				type="date"
 				className="w-full p-3 text-sm font-sans border border-primary-main-color/50 text-primary-main-color/80"
+				min={getFormDate(new Date())}
 				{...register}	
 			/>
 			<UIInputHelperText text={error} />

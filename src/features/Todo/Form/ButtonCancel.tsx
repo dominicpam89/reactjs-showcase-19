d4e2526ae9a1c7c8a56009239b4f558f34a391a8
dominicpam89 <dominicpam89@gmail.com>
2023-12-18 15:55:46 +0700

    open close extension refactor for ButtonCancel && ButtonSubmit in Todo/Form/*

--- src/features/Todo/Form/ButtonCancel.tsx
+++ src/features/Todo/Form/ButtonCancel.tsx
@@ -1,12 +1,18 @@
-interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>{}
-const ButtonCancel:React.FC<Props> = ({...defaultProps}) => {
-	return <button
-		type="button"
-		className="w-full px-3 py-2 rounded-sm text-sm border border-primary-main-color text-primary-main-color"
-		{...defaultProps}
-	>
-		Cancel
-	</button>
+interface Props
+	extends React.DetailedHTMLProps<
+		React.ButtonHTMLAttributes<HTMLButtonElement>,
+		HTMLButtonElement
+	> {}
+const ButtonCancel: React.FC<Props> = ({ ...defaultProps }) => {
+	return (
+		<button
+			{...defaultProps}
+			type="button"
+			className="w-full px-3 py-2 rounded-sm text-sm border border-primary-main-color text-primary-main-color disabled:border-primary-main-color/40 disabled:text-primary-main-color/40"
+		>
+			Cancel
+		</button>
+	)
 }
 
 export default ButtonCancel
--- src/features/Todo/Form/ButtonSubmit.tsx
+++ src/features/Todo/Form/ButtonSubmit.tsx
@@ -1,8 +1,11 @@
-const ButtonSubmit = () => {
+interface Props extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement>{}
+
+const ButtonSubmit:React.FC<Props> = ({...defaultProps}) => {
 	return (
 		<button
+			{...defaultProps}
 			type="submit"
-			className="w-full px-3 py-2 rounded-sm text-sm bg-primary-main-color text-primary-main-contrast"
+			className="w-full px-3 py-2 rounded-sm text-sm bg-primary-main-color text-primary-main-contrast disabled:bg-primary-main-color/40 disabled:text-primary-main-contrast/40"
 		>
 			Submit
 		</button>
